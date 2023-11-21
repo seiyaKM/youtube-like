359d97034384bf57a9d496b7a53d66425f9ac2c6
seiyaKM <seiya.kumada.0107@gmail.com>
2023-11-21 23:51:10 +0900

    add eslint

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import List from "./components/List.vue";
 </script>
 
 <template>
   <HelloWorld msg="Vite + Vue" />
+  <List />
 </template>
