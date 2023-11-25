a64c59f85ba5b11d669a5856b42b2de7b74dbc3f
seiyaKM <seiya.kumada.0107@gmail.com>
2023-11-25 20:58:37 +0900

    add font and node type

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,4 +1,10 @@
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
+// import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import '@mdi/font/css/materialdesignicons.css';
 
-export default createVuetify();
+export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+  },
+});
