1e4989e00729d5b7c19cc83a0e21a4bd07250b79
seiyaKM <seiya.kumada.0107@gmail.com>
2023-12-02 23:09:31 +0900

    fix header style

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,17 +5,30 @@ const drawer = ref(true);
 </script>
 
 <template>
-  <v-app-bar>
-    <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-    <span>app bar</span>
-    <div>
-      <v-text-field variant="outlined"></v-text-field>
+  <v-app-bar height="56" class="main-layout__header" elevation="0">
+    <div class="d-flex align-center">
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-icon icon="mdi-youtube"></v-icon>
+      <span>YouTube</span>
+    </div>
+    <div class="d-flex align-center main-layout__search">
+      <v-text-field variant="outlined" hide-details density="compact" rounded="pill" placeholder="search">
+        <template #append-inner>
+          <v-icon icon="mdi-magnify"></v-icon>
+        </template>
+      </v-text-field>
+      <v-btn icon="mdi-microphone" color="grey-lighten-3 ml-3" variant="flat"></v-btn>
+    </div>
+    <div class="d-flex align-center">
+      <v-btn icon="mdi-video-box"></v-btn>
+      <v-btn icon="mdi-bell-badge-outline"></v-btn>
+      <v-btn icon="mdi-account"></v-btn>
     </div>
   </v-app-bar>
-  <v-navigation-drawer v-model="drawer">
-    <v-list-item link title="Home" prepend-icon="mdi-home" :to="{ name: 'home' }"></v-list-item>
-    <v-list-item link title="Short" prepend-icon="mdi-shower" :to="{ name: 'short' }"></v-list-item>
-    <v-list-item link title="Subscription" prepend-icon="mdi-subway" :to="{ name: 'subscription' }"></v-list-item>
+  <v-navigation-drawer v-model="drawer" class="pa-3">
+    <v-list-item link title="ホーム" prepend-icon="mdi-home" :to="{ name: 'home' }" class="rounded-lg"></v-list-item>
+    <v-list-item link title="ショート" prepend-icon="mdi-play-speed" :to="{ name: 'short' }" class="rounded-lg"></v-list-item>
+    <v-list-item link title="登録チャンネル" prepend-icon="mdi-playlist-play" :to="{ name: 'subscription' }" class="rounded-lg"></v-list-item>
   </v-navigation-drawer>
   <v-main>
     <div class="pa-4">
@@ -23,3 +36,17 @@ const drawer = ref(true);
     </div>
   </v-main>
 </template>
+
+<style scoped lang="scss">
+.main-layout {
+  &__header {
+    ::v-deep(.v-toolbar__content) {
+      justify-content: space-between;
+    }
+  }
+  &__search {
+    flex: 1;
+    max-width: 736px;
+  }
+}
+</style>
